6c4104e6badcf98782d6a91b2529d1ef2895bdd6
vpmedia <andras@docmet.com>
2014-09-27 17:18:17 +0200

    #1 In Progress

--- ios/ClientExtension/ClientExtension.m
+++ ios/ClientExtension/ClientExtension.m
@@ -30,29 +30,121 @@
  
 #import "ClientExtension.h"
 
-//------------------------------------
-//
-// Native Commands.
-//
-//------------------------------------
+//----------------------------------
+// Constants
+//----------------------------------
+
+// Common
+#define EXT_LOG 1
+#define EXT_DEVICE_ID 2
+#define EXT_VIBRATE 3
+#define EXT_NOTIFY 4
+#define EXT_START_SENSOR 5
+#define EXT_STOP_SENSOR 6
+#define EXT_HAS_SENSOR 7
+#define EXT_LIST_SENSOR 8
+
+//----------------------------------
+// Helpers
+//----------------------------------
+
+BOOL ExtLog() {
+    NSLog(@"ExtLog()");
+    return YES;
+}
+
+BOOL ExtDeviceId() {
+    NSLog(@"ExtDeviceId()");
+    return YES;
+}
+
+BOOL ExtVibrate() {
+    NSLog(@"ExtVibrate()");
+    return YES;
+}
+
+BOOL ExtNotify() {
+    NSLog(@"ExtNotify()");
+    return YES;
+}
+
+BOOL ExtStartSensor(int32_t sensorType) {
+    NSLog(@"ExtStartSensor()");
+    return YES;
+}
+
+BOOL ExtStopSensor(int32_t sensorType) {
+    NSLog(@"ExtStopSensor()");
+    return YES;
+}
+
+
+BOOL ExtHasSensor(int32_t sensorType) {
+    NSLog(@"ExtHasSensor()");
+    return YES;
+}
+
+
+BOOL ExtListSensor() {
+    NSLog(@"ExtListSensor()");
+    return YES;
+}
+
+//----------------------------------
+// API
+//----------------------------------
 
 FREObject callNative(FREContext ctx, void* funcData, uint32_t argc, FREObject argv[] )
 {
+    NSLog(@"callNative()");
     FREObject retVal = NULL;
-    FRENewObjectFromBool((uint32_t)NO, &retVal);    
+    int32_t type = 0;
+    BOOL isSuccess = NO;
+    if(argc > 0) {
+        FREGetObjectAsInt32(argv[0], &type);
+        switch (type) {
+            case EXT_LOG:
+                isSuccess = ExtLog();
+                break;
+            case EXT_DEVICE_ID:
+                isSuccess = ExtDeviceId();
+                break;
+            case EXT_VIBRATE:
+                isSuccess = ExtVibrate();
+                break;
+            case EXT_NOTIFY:
+                isSuccess = ExtNotify();
+                break;
+            case EXT_START_SENSOR:
+                isSuccess = ExtStartSensor(0);
+                break;
+            case EXT_STOP_SENSOR:
+                isSuccess = ExtStopSensor(0);
+                break;
+            case EXT_HAS_SENSOR:
+                isSuccess = ExtHasSensor(0);
+                break;
+            case EXT_LIST_SENSOR:
+                isSuccess = ExtListSensor();
+                break;
+            default:
+                isSuccess = NO;
+                break;
+        }
+    }
+    FRENewObjectFromBool((uint32_t)isSuccess, &retVal);
     return retVal;
 }
 
-//------------------------------------
-//
-// Required Methods.
-//
-//------------------------------------
+//----------------------------------
+// FREExtension
+//----------------------------------
 
 void ContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx,
                         uint32_t* numFunctionsToTest, const FRENamedFunction** functionsToSet) 
 {    
-    *numFunctionsToTest = 1;    
+    NSLog(@"ContextInitializer()");
+    *numFunctionsToTest = 1;
     FRENamedFunction* func = (FRENamedFunction*) malloc(sizeof(FRENamedFunction) * 6);
     func[0].name = (const uint8_t*) "callNative";
     func[0].functionData = NULL;
@@ -61,26 +153,20 @@ void ContextInitializer(void* extData, const uint8_t* ctxType, FREContext ctx,
 }
 
 void ContextFinalizer(FREContext ctx) {
-    
-    NSLog(@"Entering ContextFinalizer()");    
-    // TODO    
-    NSLog(@"Exiting ContextFinalizer()");    
+    NSLog(@"ContextFinalizer()");
     return;
 }
 
 void ClientExtensionInitializer(void** extDataToSet, FREContextInitializer* ctxInitializerToSet,
                     FREContextFinalizer* ctxFinalizerToSet) {    
-    NSLog(@"Entering ClientExtensionInitializer()");    
+    NSLog(@"ClientExtensionInitializer()");
     *extDataToSet = NULL;
     *ctxInitializerToSet = &ContextInitializer;
-    *ctxFinalizerToSet = &ContextFinalizer;    
-    NSLog(@"Exiting ClientExtensionInitializer()");
+    *ctxFinalizerToSet = &ContextFinalizer;
 }
 
 void ClientExtensionFinalizer(void* extData) {
     
-    NSLog(@"ClientExtensionFinalizer()");    
-    // Nothing to clean up.    
-    NSLog(@"Exiting ClientExtensionFinalizer()");
+    NSLog(@"ClientExtensionFinalizer()");
     return;
 }
